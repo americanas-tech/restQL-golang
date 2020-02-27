9dab1aa87947acd2157ca52659e678a096d5d1e7
Caio Ferreira <caiorcferreira@gmail.com>
2020-02-27 16:16:40 -0300

    feat(conf): implement support for configuration file and access environment variables

--- go.mod
+++ go.mod
@@ -4,5 +4,8 @@ go 1.13
 
 require (
 	github.com/alecthomas/participle v0.4.1
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/pkg/errors v0.9.1
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/yaml.v2 v2.2.8
 )
---
+++ internal/plataform/conf/conf.go
@@ -0,0 +1,100 @@
+package conf
+
+import (
+	"errors"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"sync"
+)
+
+const configFileName = "restql.yml"
+
+var once sync.Once
+var fs FileSource
+
+type Config struct {
+	fs  *FileSource
+	env EnvSource
+}
+
+func New() (Config, error) {
+	once.Do(func() {
+		readConfigFile(&fs)
+	})
+
+	return Config{fs: &fs, env: EnvSource{}}, nil
+}
+
+func (c Config) File() *FileSource {
+	return c.fs
+}
+
+type FileSource struct {
+	data []byte
+}
+
+func (fs *FileSource) Unmarshal(target interface{}) error {
+	if len(fs.data) == 0 {
+		return errors.New("no file data present, you must have a config file")
+	}
+
+	return yaml.Unmarshal(fs.data, target)
+}
+
+func (c Config) Env() EnvSource {
+	return c.env
+}
+
+type EnvSource struct{}
+
+func (e EnvSource) GetString(key string) string {
+	return os.Getenv(key)
+}
+
+func readConfigFile(fs *FileSource) {
+	path := getConfigFilepath()
+	if path == "" {
+		log.Printf("[WARN] no config file present")
+		return
+	}
+
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Printf("[ERROR] could not load file at %s", path)
+		return
+	}
+
+	fs.data = data
+}
+
+func getConfigFilepath() string {
+	fileAtRoot := filepath.Join(".", configFileName)
+	if doesFileExist(fileAtRoot) {
+		return fileAtRoot
+	}
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Printf("[DEBUG] failed to find home directory: %v", err)
+		return ""
+	}
+
+	fileAtHome := filepath.Join(homeDir, configFileName)
+	if doesFileExist(fileAtHome) {
+		return fileAtHome
+	}
+
+	return ""
+}
+
+func doesFileExist(filepath string) bool {
+	info, err := os.Stat(filepath)
+	if os.IsNotExist(err) {
+		return false
+	}
+
+	return !info.IsDir()
+}
