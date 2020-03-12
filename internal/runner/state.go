3f2bb1d1850d7e848fdf9aa0de3a22670d629653
Caio Ferreira <caiorcferreira@gmail.com>
2020-03-12 15:59:53 -0300

    feat(runner): implement runner state

--- internal/runner/state.go
+++ internal/runner/state.go
@@ -1,35 +1,110 @@
 package runner
 
-import "github.com/b2wdigital/restQL-golang/internal/domain"
+import (
+	"github.com/b2wdigital/restQL-golang/internal/domain"
+)
 
-type RunnerState struct {
-	todo []domain.Statement
+type ResourceId string
+type AvailableResources map[ResourceId]interface{}
+type RequestedResources map[ResourceId]interface{}
+type DoneResources map[ResourceId]interface{}
+
+type DoneRequest domain.Response
+type DoneRequests []DoneRequest
+
+type State struct {
+	todo      map[ResourceId]interface{}
+	requested RequestedResources
+	done      DoneResources
 }
 
-func NewRunnerState(query domain.Query) *RunnerState {
-	return &RunnerState{todo: query.Statements}
+func NewState(query domain.Query) *State {
+	todo := makeTodoResources(query)
+
+	return &State{todo: todo, requested: make(RequestedResources), done: make(DoneResources)}
 }
 
-func (rs *RunnerState) GetAvailableRequests() []domain.Statement {
-	var available []domain.Statement
-	for _, stmt := range rs.todo {
-		if rs.canRequest(stmt) {
-			available = append(available, stmt)
+func makeTodoResources(query domain.Query) map[ResourceId]interface{} {
+	todo := make(map[ResourceId]interface{})
+	for _, stmt := range query.Statements {
+		index := getResourceId(stmt)
+		current := todo[index]
+
+		switch current := current.(type) {
+		case []domain.Statement:
+			todo[index] = append(current, stmt)
+		case domain.Statement:
+			list := []domain.Statement{current, stmt}
+			todo[index] = list
+		default:
+			todo[index] = stmt
+		}
+
+	}
+
+	return todo
+}
+
+func (s *State) Available() AvailableResources {
+	available := make(AvailableResources)
+	for key, stmt := range s.todo {
+		switch stmt := stmt.(type) {
+		case domain.Statement:
+			if s.canRequest(stmt) {
+				available[key] = stmt
+			}
+		case []domain.Statement:
+			if s.canRequest(stmt[0]) {
+				available[key] = stmt
+			}
 		}
 	}
 
 	return available
 }
 
-func (rs *RunnerState) canRequest(statement domain.Statement) bool {
+func (s *State) canRequest(statement domain.Statement) bool {
 	for _, v := range statement.With {
-		switch v.(type) {
+		switch v := v.(type) {
 		case domain.Chain:
-			return false
+			resourceTarget, ok := v[0].(string)
+			if !ok {
+				return false
+			}
+
+			_, found := s.done[ResourceId(resourceTarget)]
+			return found
 		default:
 			continue
 		}
 	}
 
 	return true
+}
+
+func (s *State) UpdateDone(resourceId ResourceId, response interface{}) {
+	s.done[resourceId] = response
+	delete(s.requested, resourceId)
+}
+
+func (s *State) Done() DoneResources {
+	return s.done
+}
+
+func (s *State) SetAsRequest(resourceId ResourceId) {
+	statement := s.todo[resourceId]
+	s.requested[resourceId] = statement
+	delete(s.todo, resourceId)
+}
+
+func (s *State) Requested() RequestedResources {
+	return s.requested
+}
+
+func getResourceId(statement domain.Statement) ResourceId {
+	if statement.Alias != "" {
+		return ResourceId(statement.Alias)
+	}
+
+	return ResourceId(statement.Resource)
 }
