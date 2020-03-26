99bfd168c46f309875fd385607594dd8b0454c33
Caio Ferreira <caiorcferreira@gmail.com>
2020-03-26 14:21:33 -0300

    feat(eval): implement filters based on only block with matches function

--- internal/eval/filters.go
+++ internal/eval/filters.go
@@ -1,13 +1,14 @@
 package eval
 
 import (
+	"fmt"
 	"github.com/b2wdigital/restQL-golang/internal/domain"
+	"github.com/pkg/errors"
+	"regexp"
 	"strings"
 )
 
-var Leaf interface{} = nil
-
-func ApplyFilters(query domain.Query, resources domain.Resources) domain.Resources {
+func ApplyFilters(query domain.Query, resources domain.Resources) (domain.Resources, error) {
 	result := make(domain.Resources)
 
 	for _, stmt := range query.Statements {
@@ -16,62 +17,124 @@ func ApplyFilters(query domain.Query, resources domain.Resources) domain.Resourc
 		}
 		resourceId := domain.NewResourceId(stmt)
 		dr := resources[resourceId]
-		result[resourceId] = applyOnlyFilters(stmt.Only, dr)
+
+		filtered, err := applyOnlyFilters(stmt.Only, dr)
+		if err != nil {
+			return nil, err
+		}
+
+		result[resourceId] = filtered
 	}
 
-	return result
+	return result, nil
 }
 
-func applyOnlyFilters(filters []interface{}, resourceResult interface{}) interface{} {
+func applyOnlyFilters(filters []interface{}, resourceResult interface{}) (interface{}, error) {
 	if len(filters) == 0 {
-		return resourceResult
+		return resourceResult, nil
 	}
 
 	switch resourceResult := resourceResult.(type) {
 	case domain.DoneResource:
+		result, err := extractWithFilters(buildFilterTree(filters), resourceResult.Result)
+		if err != nil {
+			return nil, err
+		}
+
 		return domain.DoneResource{
 			Details: resourceResult.Details,
-			Result:  extractWithFilters(buildFilterTree(filters), resourceResult.Result),
-		}
+			Result:  result,
+		}, nil
 	case domain.DoneResources:
 		list := make(domain.DoneResources, len(resourceResult))
 		for i, r := range resourceResult {
-			list[i] = applyOnlyFilters(filters, r)
+			list[i], _ = applyOnlyFilters(filters, r)
 		}
-		return list
+		return list, nil
 	default:
-		return nil
+		return nil, nil
 	}
 }
 
-func extractWithFilters(filters map[string]interface{}, resourceResult interface{}) interface{} {
+func extractWithFilters(filters map[string]interface{}, resourceResult interface{}) (interface{}, error) {
 	switch resourceResult := resourceResult.(type) {
 	case map[string]interface{}:
 		node := make(map[string]interface{})
 
 		for key, subFilter := range filters {
 			value := resourceResult[key]
-			if subFilter == Leaf {
+
+			if matchFilter, isMatch := subFilter.(domain.Match); isMatch {
+				err := applyMatchFilter(matchFilter, key, value, node)
+				if err != nil {
+					return nil, err
+				}
+			} else if subFilter == nil {
 				node[key] = value
 			} else {
 				subFilter, _ := subFilter.(map[string]interface{})
-				node[key] = extractWithFilters(subFilter, value)
+				filtered, err := extractWithFilters(subFilter, value)
+				node[key] = filtered
+				if err != nil {
+					return nil, err
+				}
 			}
 
 		}
 
-		return node
+		return node, nil
 	case []interface{}:
 		node := make([]interface{}, len(resourceResult))
 
 		for i, r := range resourceResult {
-			node[i] = extractWithFilters(filters, r)
+			filtered, err := extractWithFilters(filters, r)
+			node[i] = filtered
+			if err != nil {
+				return nil, err
+			}
 		}
 
-		return node
+		return node, nil
 	}
 
-	return nil
+	return nil, nil
+}
+
+func applyMatchFilter(filter domain.Match, key string, value interface{}, node map[string]interface{}) error {
+	switch value := value.(type) {
+	case []interface{}:
+		var list []interface{}
+		regex, err := regexp.Compile(filter.Arg)
+		if err != nil {
+			return errors.Wrap(err, "regex matching failed")
+		}
+
+		for _, v := range value {
+			strVal := fmt.Sprintf("%v", v)
+			match := regex.MatchString(strVal)
+			if match {
+				list = append(list, v)
+			}
+		}
+
+		if len(list) > 0 {
+			node[key] = list
+		}
+
+		return nil
+	default:
+		strVal := fmt.Sprintf("%v", value)
+		match, err := regexp.MatchString(filter.Arg, strVal)
+		if err != nil {
+			return errors.Wrap(err, "regex matching failed")
+		}
+
+		if match {
+			node[key] = value
+		}
+
+		return nil
+	}
 }
 
 func buildFilterTree(filters []interface{}) map[string]interface{} {
@@ -82,20 +145,34 @@ func buildFilterTree(filters []interface{}) map[string]interface{} {
 		case string:
 			path := parsePath(f)
 			buildPathInTree(path, tree)
+		case domain.Match:
+			path := parsePath(f)
+			buildPathInTree(path, tree)
 		}
 	}
 
 	return tree
 }
 
-func buildPathInTree(path []string, tree map[string]interface{}) {
+func buildPathInTree(path []interface{}, tree map[string]interface{}) {
 	if len(path) == 0 {
 		return
 	}
 
-	field := path[0]
+	var field string
+	var leaf interface{}
+
+	switch f := path[0].(type) {
+	case string:
+		field = f
+		leaf = nil
+	case domain.Match:
+		field = f.Target
+		leaf = f
+	}
+
 	if len(path) == 1 {
-		tree[field] = Leaf
+		tree[field] = leaf
 		return
 	}
 
@@ -115,6 +192,29 @@ func buildPathInTree(path []string, tree map[string]interface{}) {
 
 }
 
-func parsePath(s string) []string {
-	return strings.Split(s, ".")
+func parsePath(s interface{}) []interface{} {
+	switch s := s.(type) {
+	case string:
+		items := strings.Split(s, ".")
+
+		result := make([]interface{}, len(items))
+		for i, item := range items {
+			result[i] = item
+		}
+		return result
+	case domain.Match:
+		items := strings.Split(s.Target, ".")
+
+		result := make([]interface{}, len(items))
+		for i, item := range items {
+			if i == len(items)-1 {
+				result[i] = domain.Match{Target: item, Arg: s.Arg}
+			} else {
+				result[i] = item
+			}
+		}
+		return result
+	default:
+		return nil
+	}
 }
