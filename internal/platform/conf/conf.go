c59ba01d3f1c049f6d8f456b754edb4000dfe86d
cupello <rafacupello@gmail.com>
2020-07-28 13:01:01 -0300

    feat(conf): allow custom log timestamp format

--- internal/platform/conf/conf.go
+++ internal/platform/conf/conf.go
@@ -64,6 +64,7 @@ type Config struct {
 	Logging struct {
 		Enable    bool   `yaml:"enable" env:"RESTQL_LOGGING_ENABLE"`
 		TimestampFieldName string `yaml:"timestampFieldName"`
+		TimeFieldFormat string `yaml:"timeFieldFormat"`
 		Level     string `yaml:"level" env:"RESTQL_LOGGING_LEVEL"`
 		Format    string `yaml:"format"`
 	} `yaml:"logging"`
