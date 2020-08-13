9a43c9f9aa4db4e5a465f19157cca3aa019ad0af
Caio Ferreira <caiorcferreira@gmail.com>
2020-08-13 16:58:04 -0300

    refactor(plugins): create lifecycle plugin interface

--- internal/platform/plugins/static_loader.go
+++ internal/platform/plugins/static_loader.go
@@ -2,19 +2,26 @@ package plugins
 
 import (
 	"github.com/b2wdigital/restQL-golang/pkg/restql"
+	"github.com/pkg/errors"
 )
 
-func loadStaticPlugin(logger restql.Logger) []restql.Plugin {
-	var ps []restql.Plugin
-	for _, loader := range restql.GetPluginLoaders() {
-		p, err := loader(logger)
+func loadLifecyclePlugins(logger restql.Logger) []restql.LifecyclePlugin {
+	var ps []restql.LifecyclePlugin
+	for _, pluginInfo := range restql.LifecyclePlugins() {
+		p, err := pluginInfo.New(logger)
 		if err != nil {
 			logger.Error("failed to load plugin", err)
 			continue
 		}
 
-		logger.Debug("plugin loaded", "name", p.Name())
-		ps = append(ps, p)
+		pluginInstance, ok := p.(restql.LifecyclePlugin)
+		if !ok {
+			logger.Error("failed to load plugin", errors.Errorf("plugin of incorrect type: %T", p))
+			continue
+		}
+
+		logger.Debug("plugin loaded", "name", pluginInstance.Name())
+		ps = append(ps, pluginInstance)
 	}
 	return ps
 }
--- internal/platform/web/middleware/transaction.go
+++ internal/platform/web/middleware/transaction.go
@@ -6,29 +6,29 @@ import (
 )
 
 type Transaction struct {
-	pluginsManager plugins.Manager
+	lifecycle plugins.Lifecycle
 }
 
-func NewTransaction(pm plugins.Manager) Middleware {
-	return Transaction{pluginsManager: pm}
+func NewTransaction(l plugins.Lifecycle) Middleware {
+	return Transaction{lifecycle: l}
 }
 
 func (t Transaction) Apply(h fasthttp.RequestHandler) fasthttp.RequestHandler {
 	return func(ctx *fasthttp.RequestCtx) {
 		nativeContext := GetNativeContext(ctx)
 
-		transactionCtx := t.pluginsManager.RunBeforeTransaction(nativeContext, ctx)
+		transactionCtx := t.lifecycle.BeforeTransaction(nativeContext, ctx)
 		WithNativeContext(ctx, transactionCtx)
 
 		defer func() {
 			if reason := recover(); reason != nil {
-				t.pluginsManager.RunAfterTransaction(transactionCtx, ctx)
+				t.lifecycle.AfterTransaction(transactionCtx, ctx)
 				panic(reason)
 			}
 		}()
 
 		h(ctx)
 
-		t.pluginsManager.RunAfterTransaction(transactionCtx, ctx)
+		t.lifecycle.AfterTransaction(transactionCtx, ctx)
 	}
 }
